e77a216a648a85c327e6d524c3e29990e515c6d2
Namjin-kimm <namjin-kim@daum.net>
2022-09-19 17:09:23 +0900

    JUnit Test

---
+++ src/test/java/com/goodee/market/MemberDAOTest.java
@@ -0,0 +1,14 @@
+package com.goodee.market;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MemberDAOTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
